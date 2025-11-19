aac8b8692c56011d288e331e90357187e64c6bb3
leonardo <leomarcos.b10@gmail.com>
2025-11-19 16:45:08 -0300

    front-19-11-25

---
+++ front_orientado/src/App.jsx
@@ -0,0 +1,69 @@
+import './App.css'
+import apiLocal from './Api/apiLocal'
+
+
+export default function App() {
+
+  async function consultaFuncionarios() {
+   try {
+     const resposta = await apiLocal.get('/VisualizarFuncionarios')
+    console.log(resposta.data)
+   } catch (err) {
+    console.log(err.response)
+   }
+  }
+
+  async function cadastrarFuncionarios() {
+    //try catch - proteção do site, ao passar informações de facil compreensão
+    try {
+      const cpf = '111111111'
+      const idHierarquia = 'd5c192dc-4bea-4282-9be4-ca52161f34c1'
+      const nome = 'leonardo'
+      const email = 'leonardo@teste.co.br'
+      const senha = '12345'
+      const status = true
+
+      const resposta = await apiLocal.post('/CadastrarFuncionarios', {
+        nome,
+        cpf,
+        email,
+        senha,
+        status,
+        idHierarquia
+      })
+      console.log(resposta.data.dados)
+    } catch (err) {
+        console.log(err.response.data.error)
+    }
+
+  }
+
+  async function apagarFuncionarios() {
+    const id = 'd5c192dc-4bea-4282-9be4-ca52161f34c1'
+    const resposta = await apiLocal.delete()
+  }
+
+  //d5c192dc-4bea-4282-9be4-ca52161f34c1g
+
+
+  return (
+    <>
+      <div>
+        <center>
+          <button onClick={apagarFuncionarios}    >Apagar</button>
+          <button onClick={consultaFuncionarios}  >Consultar</button>
+          <button onClick={cadastrarFuncionarios} >Cadastrar</button>
+        </center>
+      </div>
+    </>
+
+  )
+}
+
+
+
+
+
+
+
+
